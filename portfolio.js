e52c3887b8e91944a4b49937b2191807cd97fa14
Shubh118 <npandey5ideal@gmail.com>
2022-09-17 12:02:25 +0530

    p

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,24 @@
-let image = document.querySelector("#project>div>img");
+let image = document.querySelector("#project>div>a>img");
+let link = document.querySelector("#project>div>a");
 let left = document.querySelector("#project>i");
 let right = document.querySelector("#project>div+i");
-let  arr = ["img/dummy2.png","img/dumy1.png"];
+let  arr = ["img/bella vita.png","img/dot and key.png"];
+let links = ["https://enchanting-bubblegum-e67cc4.netlify.app/index.html","https://benevolent-paletas-bb73f5.netlify.app/"];
 let i = 0;
 
 left.addEventListener("click",() => {
     if(i>0){
         i = i-1;
         image.src = arr[i];
+        link.setAttribute( "href", links[i]);
     }  
-   
 });
 
 right.addEventListener("click",() => {
     if(i<arr.length-1) {
         i = i+1;
         image.src = arr[i];
+        link.setAttribute( "href", links[i]);
     } 
 });
 
@@ -24,5 +27,6 @@ setInterval(() =>  {
     if(i >= arr.length-1) i = -1;
 
     image.src = arr[++i];
+    link.setAttribute( "href", links[++i]);
     
 },4000);
