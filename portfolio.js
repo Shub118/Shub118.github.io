eb70ead8c3761c5179ac8c696e11abaff8ac47fb
Shubh118 <npandey5ideal@gmail.com>
2022-09-20 11:38:41 +0530

    p

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,11 @@
-let image = document.querySelector("#project>div>a>img");
-let link = document.querySelector("#project>div>a");
+let image = document.querySelector("#project>div>div>img");
+let link = document.querySelector("#prl");
+let git = document.querySelector("#prg");
 let left = document.querySelector("#project>i");
 let right = document.querySelector("#project>div+i");
 let  arr = ["img/bella vita.png","img/dot and key.png"];
 let links = ["https://enchanting-bubblegum-e67cc4.netlify.app/index.html","https://benevolent-paletas-bb73f5.netlify.app/"];
+let gitlink = ["https://benevolent-paletas-bb73f5.netlify.app/","https://github.com/Shub118/cute-price-9116"];
 let i = 0;
 
 left.addEventListener("click",() => {
@@ -11,6 +13,7 @@ left.addEventListener("click",() => {
         i = i-1;
         image.src = arr[i];
         link.setAttribute( "href", links[i]);
+        git.setAttribute( "href", gitlink[i]);
     }  
 });
 
@@ -19,6 +22,26 @@ right.addEventListener("click",() => {
         i = i+1;
         image.src = arr[i];
         link.setAttribute( "href", links[i]);
+        git.setAttribute( "href", gitlink[i]);
     } 
 });
+
+
+
+// //////////////////////////////////////////////////////////////////////////////////////
+  function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "npandey5ideal@gmail.com",
+	Password : "9998647141",
+	To : 'npandey5ideal@gmail.com',
+	From : document.querySelector("#user_email").value,
+	Subject : "hiring",
+	Body : document.querySelector("#message").value,
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
+  
+
 
